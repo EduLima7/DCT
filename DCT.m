e3b0003637a6a17f481f8daa66b1acf6912fcbe4
EduLima7 <edu_a7x@hotmail.com.br>
2016-04-15 00:35:46 -0300

    Update DCT.m
    Código finalizado.

--- DCT.m
+++ DCT.m
@@ -1,31 +1,36 @@
-clear; clc;
+close all; clear; clc;
 pkg load signal;
 pkg load image;
-fullPath = ('C:\Users\Edu\Desktop\Nova pasta\Imagem para teste\couple.tiff');
+fullPath = ('C:\Users\Edu\Desktop\Nova pasta\Imagens para teste\Treino\4.2.07.tiff');
 A = imread(fullPath);
-
 figure, imshow(A)
 
-#Armazena as dimensões da imagem lida em x e y 
-[x,y] = size(A);
+#Armazena as dimensões da imagem em x,y,z. z será os canais, caso a imagem não seja em tons de cinza
+[x,y,z] = size(A); 
 
-#Iniciando matrizes de zeros com dimensão igual a da imagem
-B = uint8(zeros(x,y));   C=B;
+#Usaremos a matriz B para manipular a imagem
+B = uint8(zeros(x,y,z)); 
 
-#Dimensão dos blocos que são aplicados DCT
-M = 8;    N = 8;
+if(z>1)
+  B = rgb2gray(A);
+  figure, imshow(B)
+end
+
+
+#Dimensão dos blocos que serão aplicados DCT
+M = 16;    N = 16;
 
 #Fator de Qualidade
 F = 2; 
-
-#Criando a matriz de quantização Q
+#Criando a Matriz de Quantização
 for i = 1:M
   for j = 1:N
     Q(i,j) = 1+(1+i+j)*F;
   end
 end
 
-#Percorrendo a matriz com blocos MxN e aplicando a dct em cada bloco, logo em seguida é aplicada a inversa da dct
+#Percorrendo a matriz com blocos MxN e aplicando a dct em cada bloco, logo em seguida 
+#é aplicada a inversa da dct
 for i = 1:x/M,
   for j = 1:y/N,
     B = A(M*i-(M-1):M*i,N*j-(N-1):N*j);
@@ -35,11 +40,12 @@ for i = 1:x/M,
     B = B.*Q;
     B = idct2(B);
     B = round(B);
-    C(M*i-(M-1):M*i,N*j-(N-1):N*j)= B;
+    A(M*i-(M-1):M*i,N*j-(N-1):N*j)= B;
    end
 end
 
 
-figure,imshow(C)
+#Exibe o resultado e salva o arquivo de imagem no formato JPEG
+figure,imshow(A)
 
-imwrite(C,'C:\Users\Edu\Desktop\Nova pasta\Imagem para teste\couple-compress.JPEG');
+imwrite(A,'C:\Users\Edu\Desktop\Nova pasta\Imagens para teste\Compressed\image.JPEG');
